b831788eceb579f744c68f2fb9b2fe19089fab09
hadjimylos <chris.hadjimylos@bitbytebuffer.com>
2018-10-17 22:20:32 +0300

    remove entity framework nuget
    remove stale ef context reference
    
    remove stale ef context reference

--- resume/Startup.cs
+++ resume/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NetCoreTemplate.Models.Context;
 
 namespace NetCoreTemplate
 {
